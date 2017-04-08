4ee69a39998912ad1f97f49b275641d1b0e64d8a
Vegewong <wanghanjingwhj@gmail.com>
2017-04-09 01:41:27 +0800

    Stopped->SLEEP!

--- gitlearning/Vegewong/Interface.hpp
+++ gitlearning/Vegewong/Interface.hpp
@@ -1,15 +1,52 @@
 #include <fstream>
 
+class Ticket()
+{
+ private:
+    string Ticket_Start;
+    string Ticket_Determination;
+    string Ticket_TrainNumber;
+
+    int Ticket_Level;
+    int Ticket_Price;
+    int Ticket_Remain;
+    int Ticket_Total;
+    int Ticket_AllreadySold;
+    
+    Tree *Ticket_AllreadyBoughtUser;
+}
+
+template <class Typename>
+class GeneralNode()
+{
+ private:
+    Typename data;
+    GeneralNode *GeneralNode_NextGen;
+    GeneralNode *GeneralNode_NextBro;
+
+ public:
+    GeneralNode(){}
+    ~GeneralNode(){}
+    Typename GetStorage();                  //取出存储的data信息
+    
+
+
+}
 class Account               //账户类基类
 {
  private:
     const string Account_ID{};      //账户ID
     string Account_NickName;//
     string Account_Password;
+    
+    bool Account_isAdmin; 
  
  public:
     Account() = 0;
     ~Account() = 0;
+    bool isAdmin();
+    bool ChangeNickName(const string &Account_NewNickName);
+    bool ChangePassword(const string &Account_OldPassword, const string &Account_NewPassword);
 }
 
 class User:public Account   //用户账户类
@@ -21,7 +58,5 @@ class User:public Account   //用户账户类
  public:
     User(){}                 //建立用户账户
     ~User(){}                //用户帐户析构函数
-    bool ChangePassword(const string &NewPassword);
-    bool ChangeUserNickName(const string &NewNickName)
-
+    bool OrderTickets
 }
