5218dc0a0c9ff8107a5194fb96db02fb3aaa8240
Vegewong <wanghanjingwhj@gmail.com>
2017-04-13 11:41:35 +0800

    File

---
+++ gitlearning/Vegewong/File.hpp
@@ -0,0 +1,62 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class FileModify
+{
+ private:
+ 	fstream file_ptr;
+ public:
+	string file;
+	FileModify(const string &fname ):file(fname){}
+	void reset();
+	string read();
+	void write( const string &data );
+	void open();
+	void close();
+};
+
+void FileModify::open()
+{
+	if(file_ptr.good())
+		file_ptr.open(file.c_str());
+		cout << file <<endl;
+}
+
+void FileModify::close()
+{
+	if(file_ptr.good())
+		file_ptr.close();
+}
+
+void FileModify::reset()
+{
+	if(file_ptr.good())
+	{
+		file_ptr.close();
+		file_ptr.open(file.c_str(), ios::trunc | ios::in | ios::out);
+	}
+}
+
+string FileModify::read()
+{
+	fstream file_out(file.c_str());
+	string string_read;
+	string tmp;
+	
+	getline(file_out, string_read);
+	while(getline(file_out, tmp))
+	{
+		string_read += '\n';
+		string_read += tmp;
+	}
+	file_out.close();
+	return string_read;
+	
+}
+
+void FileModify::write(const string &data)
+{
+	file_ptr << data;
+}
