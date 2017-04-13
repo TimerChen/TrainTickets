401a0e87395674a20472f5c113dea91f4055dff1
Michael Wu <wz.wzh@qq.com>
2017-04-13 09:23:56 +0800

    SmartPoint is available, vector is not fully changed to smartpoint

--- gitlearning/MW/SmartPoint_And_Vector/smartpoint.hpp
+++ gitlearning/MW/SmartPoint_And_Vector/smartpoint.hpp
@@ -1,69 +1,60 @@
 #ifndef SMART_POINT
 #define SMART_POINT
 
-Template<typename T>
-class SmartPoint;
-Template<typename T>
-class SmartPoint_Base {
-	friend class SmartPoint<T>;
-private:
-	int cnt;
-	T *baseptr;
-	SmartPoint_Base(T *_ptr):baseptr(_ptr),cnt(1){}
-	void AddNewPoint(){++cnt;}
-	void distruction(){
-		--cnt;
-		if (cnt <= 0) ~SmartPoint_Base();
-	}
-	~SmartPoint_Base(){
-		delete[]ptr;
-	}
+template <typename T, bool array> class SmartPoint;
+template <typename T, bool array> class SmartPoint_Base {
+    friend class SmartPoint<T, array>;
+
+  private:
+    int cnt;
+    T *baseptr;
+    SmartPoint_Base(T *_ptr) : baseptr(_ptr), cnt(1) {}
+    void AddNewPoint() { ++cnt; }
+    void distruction() {
+        --cnt;
+        if (cnt <= 0) this->~SmartPoint_Base();
+    }
+    ~SmartPoint_Base() {
+        if (array)
+            delete[] baseptr;
+        else
+            delete baseptr;
+    }
 };
 
-Template<typename T>
-class SmartPoint {
-	SmartPoint_Base<T> * base;
-public:
-	SmartPoint():base(nullptr){}
-	SmartPoint(T* ptr):base(new SmartPoint_Base<T> (ptr)){}
-	SmartPoint(const SmartPoint &rs) {
-		base = rs.base;
-		base->AddNewPoint();
-	}
-	T& operator*(){
-		return *(base->baseptr);
-	}
-	T& operator[](int i){
-		return (base->baseptr)[i];
-	}
-	T* operator->(){
-		return base->baseptr;
-	}
-	SmartPoint& operator=(const SmartPoint &rs) {
-		if (!base) {
-			base = rs.base;
-			base->AddNewPoint();
-		}
-		else {
-			base->distruction();
-			base = rs.base;
-			base->AddNewPoint();
-		}
-		return *this;
-	}
-	SmartPoint& operator=(const T *rt) {
-		if (base){
-			base->distruction();
-		}
-		base = new SmartPoint_Base<T>(rt);
-		return *this;
-	}
-	
-	~SmartPoint(){
-		base->distruction();
-	}
+template <typename T, bool array = false> class SmartPoint {
+    SmartPoint_Base<T, array> *base;
+
+  public:
+    SmartPoint() : base(nullptr) {}
+    SmartPoint(T *ptr) : base(new SmartPoint_Base<T, array>(ptr)) {}
+    SmartPoint(const SmartPoint &rs) {
+        base = rs.base;
+        base->AddNewPoint();
+    }
+    T &operator*() { return *(base->baseptr); }
+    T &operator[](int i) { return (base->baseptr)[i]; }
+    T *operator->() { return base->baseptr; }
+    SmartPoint &operator=(const SmartPoint &rs) {
+        if (!base) {
+            base = rs.base;
+            base->AddNewPoint();
+        } else {
+            base->distruction();
+            base = rs.base;
+            base->AddNewPoint();
+        }
+        return *this;
+    }
+    SmartPoint &operator=(const T *rt) {
+        if (base) {
+            base->distruction();
+        }
+        base = new SmartPoint_Base<T, array>(rt);
+        return *this;
+    }
+
+    ~SmartPoint() { base->distruction(); }
 };
 
-
-
-#endif SMART_POINT
+#endif
--- gitlearning/MW/SmartPoint_And_Vector/test.cpp
+++ gitlearning/MW/SmartPoint_And_Vector/test.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
-#include "vector.hpp"
 #include "smartpoint.hpp"
+#include "vector.hpp"
+#include <iostream>
 using namespace std;
 using namespace sjtu;
 
-int main(){
-	vector<int> a;
-	for(int i = 0; i != 10; ++i) {
-		a.push_back(i);
-	} 
+int main() {
+    vector<int> a;
+    for (int i = 0; i != 10; ++i) {
+        a.push_back(i);
+    }
 
-	SmartPoint<vector<int> > p;
-	p->push_back(2);
-	for (vector<int>::iterator it = (*p).begin(); it != (*p).end();++it){
-		cout << *it << endl;
-	}
-	return 0;
+    SmartPoint<vector<int>> p = &a;
+    p->push_back(2);
+    for (vector<int>::iterator it = (*p).begin(); it != (*p).end(); ++it) {
+        cout << *it << endl;
+    }
+    return 0;
 }
