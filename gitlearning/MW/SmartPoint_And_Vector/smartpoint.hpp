8ea00622b2c197122f78fb6e308ca304a1de3e06
Michael Wu <wz.wzh@qq.com>
2017-04-18 00:24:47 +0800

    Add const_shared_ptr and const_normal_ptr, which are not yet tested

--- gitlearning/MW/SmartPoint_And_Vector/smartpoint.hpp
+++ gitlearning/MW/SmartPoint_And_Vector/smartpoint.hpp
@@ -1,14 +1,24 @@
 #ifndef SMART_POINT
 #define SMART_POINT
 
+class not_an_array
+{
+};
+
+template <typename T>
+class const_normal_ptr;
 template <typename T, bool array, bool opnew>
 class shared_ptr;
 template <typename T>
 class normal_ptr;
+template <typename T, bool array = false, bool opnew = false>
+class const_shared_ptr;
+
 template <typename T, bool array, bool opnew>
 class shared_ptr_Base
 {
     friend class shared_ptr<T, array, opnew>;
+    friend class const_shared_ptr_Base<T, array, opnew>;
 
   private:
     int cnt;
@@ -30,7 +40,7 @@ class shared_ptr_Base
     {
         return baseptr[i];
     }
-    const T& operator[](int i) const
+    T& operator[](int i) const
     {
         return *baseptr[i];
     }
@@ -47,6 +57,41 @@ class shared_ptr_Base
     }
 };
 
+template <typename T, bool array, bool opnew>
+class const_shared_ptr_Base
+{
+
+  private:
+    shared_ptr_Base* sb;
+    const T* baseptr;
+    const_shared_ptr_Base(T* _ptr,
+                          const shared_ptr_Base<T, array, opnew>* _sb = nullptr)
+      : baseptr(_ptr)
+    {
+        if (!_sb) {
+            sb = new shared_ptr_Base<T, array, opnew>(ptr);
+        } else
+            sb = _sb;
+    }
+    void AddNewPoint()
+    {
+        sb->AddNewPoint();
+    }
+    void distruction()
+    {
+        sb->distruction();
+        if (sb->cnt <= 0)
+            delete sb;
+    }
+    const T& operator[](int i) const
+    {
+        return *baseptr[i];
+    }
+    ~const_shared_ptr_Base()
+    {
+    }
+};
+
 template <typename T, bool array = false, bool opnew = false>
 class shared_ptr
 {
@@ -57,11 +102,11 @@ class shared_ptr
       : base(nullptr)
     {
     }
-    shared_ptr(T* ptr)
+    shared_ptr(T* const ptr)
       : base(new shared_ptr_Base<T, array, opnew>(ptr))
     {
     }
-    shared_ptr(const shared_ptr& rs)
+    shared_ptr(const shared_ptr<T, array, opnew>& rs)
     {
         base = rs.base;
         base->AddNewPoint();
@@ -74,7 +119,7 @@ class shared_ptr
     {
         return (*base)[i];
     }
-    const T& operator[](int i) const
+    T& operator[](int i) const
     {
         return (*base)[i];
     }
@@ -82,7 +127,11 @@ class shared_ptr
     {
         return base->baseptr;
     }
-    shared_ptr& operator=(const shared_ptr& rs)
+    T* operator->() const
+    {
+        return base->baseptr;
+    }
+    shared_ptr& operator=(const shared_ptr<T, array, opnew>& rs)
     {
         if (!base) {
             base = rs.base;
@@ -116,31 +165,16 @@ class shared_ptr
     }
     normal_ptr<T> operator+(const int pos)
     {
+        if (!array)
+            throw not_an_array();
         return normal_ptr<T>(base->baseptr + pos);
     }
-    normal_ptr<T> operator-(const int pos)
+    const normal_ptr<T> operator+(const int pos) const
     {
-        return normal_ptr<T>(base->baseptr - pos);
+        if (!array)
+            throw not_an_array();
+        return normal_ptr<T>(base->baseptr + pos);
     }
-    normal_ptr<T> operator+=(const int pos)
-    {
-        normal_ptr<T> tmp(base->baseptr + pos);
-        this->~shared_ptr();
-        return tmp;
-    }
-    normal_ptr<T> operator-=(const int pos)
-    {
-        normal_ptr<T> tmp(base->baseptr - pos);
-        this->~shared_ptr();
-        return tmp;
-    }
-    normal_ptr<T> operator++()
-    {
-        normal_ptr<T> tmp(base->baseptr + 1);
-        this->~shared_ptr();
-        return tmp;
-    }
-
     ~shared_ptr()
     {
         base->distruction();
@@ -149,9 +183,92 @@ class shared_ptr
     }
 };
 
+template <typename T, bool array = false, bool opnew = false>
+class const_shared_ptr
+{
+    const_shared_ptr_Base<T, array, opnew>* base;
+
+  public:
+    const_shared_ptr()
+      : base(nullptr)
+    {
+    }
+    const_shared_ptr(T* const ptr)
+      : base(new const_shared_ptr_Base<T, array, opnew>(ptr))
+    {
+    }
+    const_shared_ptr(const shared_ptr<T, array, opnew>& rs)
+    {
+        base = new const_shared_ptr_Base(rs.base->baseptr, rs.base);
+        base->AddNewPoint();
+    }
+    const_shared_ptr(const const_shared_ptr<T, array, opnew>& rs)
+    {
+        base = new const_shared_ptr_Base(rs.base->baseptr, rs.base);
+        base->AddNewPoint();
+    }
+    const T& operator*() const
+    {
+        return *(base->baseptr);
+    }
+    const T& operator[](int i) const
+    {
+        return (*base)[i];
+    }
+    const T* operator->() const
+    {
+        return base->baseptr;
+    }
+
+    const_shared_ptr& operator=(const const_shared_ptr<T, array, opnew>& rs)
+    {
+        if (base) {
+            base->distruction();
+            delete base;
+        }
+        base = rs.base;
+        base->AddNewPoint();
+        return *this;
+    }
+    const_shared_ptr& operator=(const shared_ptr<T, array, opnew>& rs)
+    {
+        base = new const_shared_ptr_Base(rs.base->baseptr, rs.base);
+        base->AddNewPoint();
+    }
+    const_shared_ptr& operator=(T* rt)
+    {
+        if (base) {
+            base->distruction();
+            delete base;
+        }
+        base = new const_shared_ptr_Base<T, array, opnew>(rt);
+        return *this;
+    }
+    bool operator==(const const_shared_ptr& rt) const
+    {
+        return base == rt.base;
+    }
+    bool operator!=(const const_shared_ptr& rt) const
+    {
+        return base != rt.base;
+    }
+    const_normal_ptr<T> operator+(const int pos) const
+    {
+        if (!array)
+            throw not_an_array();
+        return const_normal_ptr<T>(base->baseptr + pos);
+    }
+    ~const_shared_ptr()
+    {
+        base->distruction();
+        delete base;
+    }
+};
+
 template <typename T>
 class normal_ptr
 {
+    friend class const_normal_ptr;
     T* ptr;
 
   public:
@@ -163,19 +280,19 @@ class normal_ptr
     {
         ptr = rp.ptr;
     }
-    bool operator==(const normal_ptr& other)
+    bool operator==(const normal_ptr& other) const
     {
         return other.ptr == ptr;
     }
-    bool operator!=(const normal_ptr& other)
+    bool operator!=(const normal_ptr& other) const
     {
         return other.ptr != ptr;
     }
-    normal_ptr<T> operator+(const int pos)
+    normal_ptr<T> operator+(const int pos) const
     {
         return normal_ptr<T>(ptr + pos);
     }
-    normal_ptr<T> operator-(const int pos)
+    normal_ptr<T> operator-(const int pos) const
     {
         return normal_ptr<T>(ptr - pos);
     }
@@ -222,4 +339,80 @@ class normal_ptr
     }
 };
 
+template <typename T>
+class const_normal_ptr
+{
+    const T* ptr;
+
+  public:
+    const_normal_ptr(const T* rp = nullptr)
+      : ptr(rp)
+    {
+    }
+    const_normal_ptr(const normal_ptr& rp)
+      : ptr(rp.ptr)
+    {
+    }
+    const_normal_ptr& operator=(const const_normal_ptr& rp)
+    {
+        ptr = rp.ptr;
+    }
+    bool operator==(const const_normal_ptr& other) const
+    {
+        return other.ptr == ptr;
+    }
+    bool operator!=(const const_normal_ptr& other) const
+    {
+        return other.ptr != ptr;
+    }
+    const_normal_ptr<T> operator+(const int pos) const
+    {
+        return const_normal_ptr<T>(ptr + pos);
+    }
+    const_normal_ptr<T> operator-(const int pos) const
+    {
+        return const_normal_ptr<T>(ptr - pos);
+    }
+    const_normal_ptr<T>& operator+=(const int pos)
+    {
+        ptr += pos;
+        return *this;
+    }
+    const_normal_ptr<T>& operator-=(const int pos)
+    {
+        ptr -= pos;
+        return *this;
+    }
+    const_normal_ptr<T>& operator++()
+    {
+        ++ptr;
+        return *this;
+    }
+    const_normal_ptr<T> operator++(int x)
+    {
+        const_normal_ptr<T> tmp(ptr);
+        ++ptr;
+        return tmp;
+    }
+    const_normal_ptr<T>& operator--()
+    {
+        --ptr;
+        return *this;
+    }
+    const_normal_ptr<T> operator--(int x)
+    {
+        const_normal_ptr<T> tmp(ptr);
+        --ptr;
+        return tmp;
+    }
+
+    const T& operator*() const
+    {
+        return *ptr;
+    }
+    const T* operator->() const
+    {
+        return ptr;
+    }
+};
 #endif
