77be5aa95a0b994635cb1ac825224fb848048fa2
TimerChen <TimeMachine@sjtu.edu.cn>
2017-05-07 00:35:45 +0800

    fix a bug of uniqe() in algorithm0.hpp

---
+++ Server/12308_Server/src_Timer/database_base.cpp
@@ -0,0 +1,17 @@
+
+#include "DataBase.h"
+
+DataBase_Base::DataBase_Base( const std::string &Name )
+:dataBase_name( Name )
+{
+
+}
+DataBase_Base::~DataBase_Base()
+{
+
+}
+
+void DataBase_Base::loadData()
+{}
+void DataBase_Base::saveData()
+{}
