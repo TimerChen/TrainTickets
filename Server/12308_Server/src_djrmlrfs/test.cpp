d948a80db0b32eeaa688cb05e4bfdb9c181f6c39
djrmlrfs <542609160@qq.com>
2017-05-12 16:39:19 +0800

    test map speed

---
+++ Server/12308_Server/src_djrmlrfs/test.cpp
@@ -0,0 +1,35 @@
+#include "map.hpp"
+#include <ctime>
+#include "map_my.hpp"
+#include <bits/stdc++.h>
+sjtu::map<int,int> b;
+ttd::map<int,int> a;
+int temp[1111111];
+int main()
+{
+	int t1 = 0, t2 = 0, t = clock();
+	for (int i = 1; i < 1000000; ++i) {
+		temp[i] = i;
+	}
+	std::random_shuffle(temp + 1, temp + 1000000);
+	for (int i = 1; i< 1000000; i++)
+	{
+		t = clock();
+		a[temp[i]] = rand();
+		t1 += clock()-t, t = clock();
+		b[temp[i]] = rand();
+		t2 += clock()-t;
+	}
+	std::cout << t1 << ' ' << t2<<std::endl;
+	
+	t1 = t2 = 0;
+	for (int i = 1; i< 5000000; i++)
+	{
+		t = clock();
+		a.find(i);
+		t1 += clock()-t, t = clock();
+		b.find(i);
+		t2 += clock()-t;
+	}
+	std::cout << t1 << ' ' << t2<<std::endl;
+}
