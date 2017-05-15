6a533744a9841cd654f0cbd385b7a36b91805cee
Michael Wu <wz.wzh@qq.com>
2017-05-15 16:51:01 +0800

    Add autocomplete

--- Client/UI/TrainUI/stationsearch.cpp
+++ Client/UI/TrainUI/stationsearch.cpp
@@ -3,12 +3,31 @@
 #include "ui_stationsearch.h"
 #include <QMessageBox>
 #include "toserverstructs.h"
+#include <QCompleter>
+#include <QFile>
 
 StationSearch::StationSearch(ttd::shared_ptr<uistructs::nowAccount> _now,
                              QWidget *parent)
     : QDialog(parent), ui(new Ui::StationSearch), nowaccount(_now) {
     ui->setupUi(this);
     ui->dateEdit->setCalendarPopup(true);
+
+    QFile stationName(":src/stationfile");
+    stationName.open(QIODevice::ReadOnly);
+    QTextStream in(&stationName);
+    QStringList word_list;
+    QString tmps;
+    while (!in.atEnd()) {
+        in >> tmps;
+        word_list<< tmps;
+        //qDebug() << tmps << endl;
+    }
+    //word_list<<"Java"<<"C++"<<"C#"<<"PHP"<<"Perl"<<"Python"<<"Delphi"<<"Ruby";
+    QCompleter *completer = new QCompleter(word_list, this);
+    completer->setCaseSensitivity(Qt::CaseInsensitive);
+    ui->stationLineEdit->setCompleter(completer);
+    stationName.close();
+
 }
 
 StationSearch::~StationSearch() { delete ui; }
